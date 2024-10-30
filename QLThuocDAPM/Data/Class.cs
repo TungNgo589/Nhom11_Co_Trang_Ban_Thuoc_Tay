e3b8e1efce2488770ff4c3758ace43105fcaba6f
Tai2602 <nguyentai4273@gmail.com>
2024-10-31 00:34:19 +0700

    Giỏ hàng

---
+++ QLThuocDAPM/Data/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace QLThuocDAPM.Data
+{
+    public class Class
+    {
+    }
+}
--- QLThuocDAPM/obj/Debug/net8.0/QLThuocDAPM.AssemblyInfo.cs
+++ QLThuocDAPM/obj/Debug/net8.0/QLThuocDAPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLThuocDAPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73123cc415e7f58657553aaf70e663fd0b554e09")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLThuocDAPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLThuocDAPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
